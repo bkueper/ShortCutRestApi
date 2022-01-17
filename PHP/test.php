951e9fb95e13fe7f910351d56c51076954cb938b
bjarne <bjarne.kueper@web.de>
2022-01-17 13:24:58 +0100

    deleted unnessecary folders

--- PHP/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $myPDO = new PDO("pgsql:host=localhost,dbname=ShortCutDB","postgres","postgres");
-    echo "Connected to PostgreSQL with PDO"
-}catch(PDOException §e){
-    echo $e->getMessage();
-}
-    
-?>
